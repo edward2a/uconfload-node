a98ebdc0c01ab0c7d7f874d9cfb2cfdfe30f5d11
Eduardo A. Paris Penas <edward2a@gmail.com>
2018-04-11 20:26:08 +0100

    handle null objects on the iterator

--- uconfload.js
+++ uconfload.js
@@ -50,7 +50,7 @@ function loadEnv(cfg_obj){
 	for (const [key, value] of Object.entries(cfg_obj)) {
 
 		// Nested dict (object) iterator
-		if (typeof(value) === 'object') { loadEnv(value); }
+		if (typeof(value) === 'object' && value !== null) { loadEnv(value); }
 
 		else if (typeof(value) === 'string' && value.startsWith('env')) {
 
